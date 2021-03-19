5579bc7ffe388edde62997aa55fd607f992e4af9
dino9611 <aldinorahman36@gmail.com>
2021-03-19 16:20:53 +0700

    day4 add drawer sama fixed bug module drawer

--- src/navigations/HomeTab.js
+++ src/navigations/HomeTab.js
@@ -25,8 +25,15 @@ function Bookscreen({navigation, route}) {
 function Userscreen({navigation, route}) {
   // tanpa destructuring props.route
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}>
       <Text>User Screen </Text>
+      <Button title="buka drawer" onPress={() => navigation.openDrawer()} />
     </View>
   );
 }
